02e2afec708f3c14f9268dd047d525c4857c1374
BorbonPalomaresJesusArmando4-2 <a24311037@uthermosillo.edu.mx>
2025-10-30 21:11:29 -0700

    Ignorar la carpeta env

--- burritos_project/urls.py
+++ burritos_project/urls.py
@@ -15,7 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.urls import path
 from django.urls import path, include
+from django.http import HttpResponse 
+
+def bienvenida(request):
+    return HttpResponse("<h1>API de Burritos-to-go</h1><p>Visita /api/ para usar la API. </p>")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
